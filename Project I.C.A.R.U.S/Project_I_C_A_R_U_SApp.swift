65f43672cc7290e11247ae8947c35096c045b644
Samuel <samuellegg2004@icloud.com>
2021-11-30 20:42:03 +0000

    Initial Commit

---
+++ Project I.C.A.R.U.S/Project_I_C_A_R_U_SApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_I_C_A_R_U_SApp.swift
+//  Project I.C.A.R.U.S
+//
+//  Created by Samuel Legg on 30/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_I_C_A_R_U_SApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
